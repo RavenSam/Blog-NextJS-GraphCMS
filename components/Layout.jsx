53ee1e0cd9e4034125091d4563b7ffb5bd45cf60
Sam Makhlouf <maksam7dev@gmail.com>
2021-11-10 14:28:49 +0100

    added progress bar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,7 @@
 import Head from "next/head"
 import { Header } from "."
+import NextNprogress from "nextjs-progressbar"
+
 
 export default function Layout({ children }) {
    return (
@@ -7,6 +9,8 @@ export default function Layout({ children }) {
          <Head>
             <link rel="icon" href="/favicon.ico" />
          </Head>
+
+         <NextNprogress options={{ showSpinner: false }} color="#db2777" startPosition={0.3} />
 
          <Header />
 
